9c635c2b72a85e4f412d1e3a0539dc2b4e1ccbdd
Borjaarmet <borjaarmet@gmail.com>
2022-02-24 10:58:33 +0100

    increase functionality

--- src/reducer.js
+++ src/reducer.js
@@ -1,3 +1,5 @@
+import CartItem from "./CartItem";
+
 const reducer = (state, action) => {
   if (action.type === "CLEAR_CART") {
     return { ...state, cart: [] };
@@ -7,6 +9,15 @@ const reducer = (state, action) => {
       ...state,
       cart: state.cart.filter((item) => item.id !== action.payload),
     };
+  }
+  if (action.type === "INCREASE") {
+    let tempCart = state.cart.map((cartItem) => {
+      if (cartItem.id === action.payload) {
+        return { ...cartItem, amount: cartItem.amount + 1 };
+      }
+      return cartItem;
+    });
+    return { ...state, cart: tempCart };
   }
   return state;
 };
